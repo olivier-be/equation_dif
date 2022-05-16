c3aaad1925f9ebdd38491479cf3d63f8d5b59a8f
olivier-be <61296476+olivier-be@users.noreply.github.com>
2022-05-16 20:50:26 +0200

    Update Analyse.h

--- Analyse.h
+++ Analyse.h
@@ -2,23 +2,25 @@
 #include <vector>
 #ifndef UNTITLED1_ANALYSE_H
 #define UNTITLED1_ANALYSE_H
-using namespace std;
 
 class Analyse {
 
     private:
-        string equation_dif;
-        int tab_positiony[4];
-        vector<int> tab_position;
-        vector<string> separation;
-    public:
-        int get_tab_positiony;
-        string get_separation;
+        std::string equation_dif;
+    std::vector<int> tab_position;
+        std::vector<std::string> tab_valeur;
+        int value_in_tab_betewen_2_point(int pos_act,int pos_fut,int i);
+public:
         void analyse_position();
-        Analyse(string equation_dif);
+        Analyse(std::string equation_dif);
+        std::vector<int> get_tab_position();
+        std::vector<std::string> get_tab_valeur();
+        int get_tab_positiony();
+        std::vector<std::string> verif_valeur();
 
-
+    int tab_positiony[4];
 };
 
 
 #endif //UNTITLED1_ANALYSE_H
+
