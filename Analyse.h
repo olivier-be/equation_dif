8bfaa58461ae3cc6688345664f8c6b855d7374db
olivier-be <61296476+olivier-be@users.noreply.github.com>
2022-05-23 18:15:51 +0200

    Update Analyse.h

--- Analyse.h
+++ Analyse.h
@@ -5,25 +5,26 @@
 
 class Analyse {
 
-    private:
-        int tab_positiony[4];
-        int search_tab_positiony_tab_valeur[4];
-        int compteur,compteur_cal;
-        std::string equation_dif;
-        std::vector<int> tab_position;
-        std::vector<std::string> tab_valeur;
-        int research(int position_act);
-        int value_in_tab_betewen_2_point(int pos_act,int pos_fut,int i);
-        void search_tab_positiony();
-        void search_tab_positiony_tab_valeur();
+private:
+    int tab_positiony[4];
+    int tab_positiony_tab_valeur[4];
+    int compteur,compteur_cal;
+    std::string equation_dif;
+    std::vector<int> tab_position;
+    std::vector<std::string> tab_valeur;
+    std::vector <int> tab_valeur_int;
+    int research(int position_act);
+    int value_in_tab_betewen_2_point(int pos_act,int pos_fut,int i);
+    void search_tab_positiony();
+    void search_tab_positiony_tab_valeur();
 
 public:
-        void analyse_position();
-        Analyse(std::string equation_dif);
-        std::vector<int> get_tab_position();
-        std::vector<std::string> get_tab_valeur();
-        int get_tab_positiony();
-        std::vector<std::string> verif_valeur();
+    void analyse_position();
+    Analyse(std::string equation_dif);
+    std::vector<int> get_tab_position();
+    std::vector<std::string> get_tab_valeur();
+    int get_tab_positiony();
+    void verif_valeur();
 
 
 };
